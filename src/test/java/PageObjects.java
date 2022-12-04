991231c7ebeea63954ad25b51cb5afa4ab6f217d
butebaliyeva <utebaliyevabotakoz@gmail.com>
2022-12-04 23:30:46 +0600

    pageObjects upload

---
+++ src/test/java/PageObjects.java
@@ -0,0 +1,63 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PageObjects {
+
+
+        @BeforeAll
+        static void beforeAll() {
+            //   Configuration.holdBrowserOpen = true;
+            Configuration.browserSize = "1920x1080";
+            Configuration.baseUrl = "https://demoqa.com";
+        }
+
+        @Test
+        void successfulRegisterTests() {
+            String userFirstName = "Botakoz";
+            String userLastName = "Utebaliyeva";
+
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+
+
+            $("#firstName").setValue(userFirstName);
+            $("#lastName").setValue(userLastName);
+            $("#userEmail").setValue("Botakoz@gmail.com");
+            $("#gender-radio-2").parent().click();
+            $("#userNumber").setValue("7777777777");
+
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("December");
+            $(".react-datepicker__year-select").selectOption("1991");
+            $(".react-datepicker__day--017").click();
+
+            $("#subjectsInput").setValue("Maths").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+
+            $("#uploadPicture").uploadFromClasspath("img/my.jpg");
+
+            $("#currentAddress").setValue("Kazakhstan Almaty");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+
+
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text(userFirstName), text(userLastName), text("Delhi"));
+
+        }
+
+    }
+
